97bb6f4ee3df42d68be9a563d09c8e93775dc089
FierensDenis <fierens.dev@gmail.com>
2026-05-05 02:58:27 +0200

    create interface

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,13 +1,27 @@
 import { fetchStocks } from "./api/stocks.ts";
+import { ApiError, NetworkError, NotFoundError, ParseError, UnauthorizedError } from "./errors/ApiErrors.ts";
 
 try {
   displayGlobalMessage('Chargements de l\'application...')
-  await fetchStocks();
+  const data = await fetchStocks();
+  console.log(`deunsLog : `, data)
 } catch(e) {
-  displayGlobalMessage('Revenez quand l\'api fonctionnera...');
+  if (e instanceof UnauthorizedError) {
+    displayGlobalMessage('Accès non autorisé');
+  } else if (e instanceof NotFoundError) {
+    displayGlobalMessage('Données introuvables');
+  } else if (e instanceof ParseError) {
+    displayGlobalMessage('Données reçu JSON invalide');
+  } else if (e instanceof ApiError) {
+    displayGlobalMessage('Erreur serveur.');
+  } else if (e instanceof NetworkError) {
+    displayGlobalMessage('Impossible de joindre le serveur.');
+  } else {
+    displayGlobalMessage('Revenez quand l\'api fonctionnera...');
+  }
 }
 
-function displayGlobalMessage(message: string)
+function displayGlobalMessage(message: string): void
 {
   const app = document.getElementById('app')!;
   app.innerHTML = message;
